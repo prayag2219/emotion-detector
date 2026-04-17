eecaf22ad06e9f365e77a1cfa36c1af010a4dfce
prayag2219 <vladimirputinak69@gmail.com>
2026-04-17 17:08:35 +0530

    Create emotion_detection.py

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,36 @@
+import requests
+
+def emotion_detector(text_to_analyse):
+    if text_to_analyse is None or text_to_analyse.strip() == "":
+        return None
+
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+
+    input_json = {
+        "raw_document": {
+            "text": text_to_analyse
+        }
+    }
+
+    response = requests.post(url, json=input_json, headers=headers)
+
+    if response.status_code == 400:
+        return None
+
+    result = response.json()
+    emotions = result['emotionPredictions'][0]['emotion']
+
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    return {
+        'anger': emotions['anger'],
+        'disgust': emotions['disgust'],
+        'fear': emotions['fear'],
+        'joy': emotions['joy'],
+        'sadness': emotions['sadness'],
+        'dominant_emotion': dominant_emotion
+    }
